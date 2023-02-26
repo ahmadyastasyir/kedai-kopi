958585e994c7a74755a1ed8b1cf67ee55eadaba0
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2023-02-27 02:14:41 +0700

    menambah file dengan proses masih 50%

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika menu di click
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klik di luar sidebar untuk menghilangkan navbarnya
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
