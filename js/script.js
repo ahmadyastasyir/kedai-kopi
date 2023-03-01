54c187b97298f36e726d142430f40855eeabf267
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2023-03-02 00:15:33 +0700

    Menambahkan Menu Section beserta gambar gambarnya

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika menu di click
-document.querySelector("#menu").onclick = () => {
+document.querySelector("#ham-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 // klik di luar sidebar untuk menghilangkan navbarnya
-const menu = document.querySelector("#menu");
+const menu = document.querySelector("#ham-menu");
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
