a6deeeadef29375166495aea9ddbfb4852f707d7
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2023-05-12 01:30:27 +0700

    menambah beberapa fitur dari video tutor pak dika

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
-// toggle class active
+// toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
+
 // ketika menu di click
 document.querySelector("#ham-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
-// klik di luar sidebar untuk menghilangkan navbarnya
+
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+// ketika search button di click
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+// ketika shopping cart di click
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar element
 const menu = document.querySelector("#ham-menu");
+const sb = document.querySelector("#search-button");
+const scb = document.querySelector("#shopping-cart-button");
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
